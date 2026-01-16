6c893de42ee9ea96d067a9bee198cfd85213a0af
unknown <nishavkumar07@gmail.com>
2026-01-16 18:29:15 +0530

    configuring sessions

---
+++ config/session.js
@@ -0,0 +1,25 @@
+const session = require('express-session');
+const MongoStore = require('connect-mongo').default;
+
+const sessionConfig = session(
+  {
+  name:"sid",
+  secret:process.env.SECRET_KEY,     
+  resave: false,                   
+  saveUninitialized: false,  
+  
+  store: MongoStore.create({
+    mongoUrl:process.env.ATLAS_URL,
+    collection:"sessions",
+    ttl: 60 * 60 * 24 * 30
+  }),
+
+  cookie: { 
+    httpOnly:true,
+    maxAge:1000 * 60 * 60 * 24 * 30
+   },
+  })
+
+
+
+  module.exports = sessionConfig;
--- middlewares/middleware.js
+++ middlewares/middleware.js
@@ -1,9 +1,56 @@
+const bcrypt = require("bcrypt");
+const User = require("../models/user.js");
+
 function flash(req,res,next){
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
 }
 
+
+async function hashing(pass){
+    const saltRounds = 10;
+    const hashPass = await bcrypt.hash(pass,saltRounds);
+    return hashPass
+}
+
+
+async function verify(pass,passDB){
+    const check = await bcrypt.compare(pass,passDB);
+    return check;
+}
+
+function isLoggedIn(req,res,next){
+    if(req.session && req.session.user){
+        next()
+    } else {
+        // req.flash("error","Login first")
+        res.redirect("/")
+    }
+}
+
+async function user(req,res,next){
+    if (req.session.user) {
+    req.user = await User.findById(req.session.user.id);
+  }
+  next();
+}
+
+
+
+async function renew(req,res,next){
+    if (req.session) {
+    req.session.touch();
+  }
+  next();
+}
+
+
 module.exports = {
-    flash
+    flash,
+    hashing,
+    verify,
+    isLoggedIn,
+    user,
+    renew
 }
