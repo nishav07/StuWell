720a3a4cbf31a6c8ec026f347ee13597aa724ddd
unknown <nishavkumar07@gmail.com>
2026-01-16 18:18:13 +0530

    making:users db schema

---
+++ models/user.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    userName:{
+        type:String,
+        required:[true,"username required"]
+    },
+    email:{
+        type:String,
+        required:[true,"email id is reuqired"],
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    DOB:{
+        type:Date,
+        default: null
+    },
+    gender:{
+        type:String,
+        enum:['male', 'female', 'prefer-not-to-say'],
+        default: null
+    },
+    weight:{
+        type:Number,
+        default: null,
+        max:300
+    },
+    academic:{
+        type:String,
+        enum:['School', 'High School', 'College'],
+        default: null
+    }
+    
+})
+
+const User = mongoose.model("User",userSchema);
+
+module.exports = User;
