a871f5c9a897827bc5596a3f2c4b7b59111795c7
unknown <nishavkumar07@gmail.com>
2026-01-18 23:07:34 +0530

    degining home page layout

---
+++ models/dailyInput.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+const inputSchema = new mongoose.Schema({
+    userId: {
+        type:String,
+        required:true
+    },
+    water: {
+        type:Number,
+        required:[true,"water input required"]
+    },
+    junkFood: {
+        type:String,
+        enum:["yes","no"],
+        required:true
+    },
+    foodType: {
+        type:String,
+        enum:["carbs","protein","fats","balanced"],
+        required:true
+    },
+    studyHr: {
+        type:Number,
+        required:true
+    },
+    mood: {
+        type:String,
+        enum:['veryBad','Bad',"neutral","Good","veryGood"],
+        required:true
+    },
+    symptoms: {
+        type:String,
+        enum:['none','headache','fatigue','stress','anxiety','stomach_discomfort','body_pain',`low_focus`],
+        required:true
+    },
+    sleepHr: {
+        type:Number,
+        required:true
+    },
+    screentime: {
+        type:Number,
+        required:true
+    },
+    date: {
+        type:Date,
+        default:Date.now
+    }
+})
+
+const daily = mongoose.model("daily", inputSchema);
+
+module.exports = daily;
